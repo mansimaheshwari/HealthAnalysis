d8604f0a05ceba67430ea5178c7d72996ef1468f
mansi maheshwari <mansi.mnnit2018@gmail.com>
2021-10-12 16:20:21 +0530

    Revert "adding comments"
    This reverts commit a23b59dc2eb8911bb39f76af6e5ef9de1a1d31c9.

--- SparkHealthDataAnalysis/src/HealthAnalysis/DrugFrequencyByLocation.scala
+++ SparkHealthDataAnalysis/src/HealthAnalysis/DrugFrequencyByLocation.scala
@@ -7,9 +7,10 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.SaveMode
 
 
+
+
 object DrugFrequencyByLocation {
   
-//  configurations for SparkSession.builder()
    def sparkConf : SparkConf={
         val sConf=new SparkConf
         sConf.set("spark.app.name", "Drug Frequency By Location")
@@ -20,6 +21,7 @@ object DrugFrequencyByLocation {
   def main(args : Array[String]){
     
       Logger.getLogger("org").setLevel(Level.ERROR)
+      Logger.getLogger(getClass.getName).error("Drug Frequency By Location")
       
         val spark=SparkSession.builder()                      
                             .config(sparkConf)
@@ -28,15 +30,15 @@ object DrugFrequencyByLocation {
     
         
                    
-       import spark.implicits._     // to used operations on df like filter etc.
+       import spark.implicits._    
        val dfMedications=spark.read
                   .format("csv")
                   .option("path","D:/HealthDataSpark/input/medications.csv")
                   .option("header",true)
                   .schema("seqn String,noOfMedicines Integer,drugName String")  //  .option("inferSchema",true)
                   .load
-                  .filter(x=> !x.isNullAt(2))    //filter out the data where deugName is missing
-                  .rdd  //   converting to rdd so that we can use rdd specific functions like flatMapValues()
+                  .filter(x=> !x.isNullAt(2))
+                  .rdd
                   .map(x=>(x.getString(0),x.getString(2)))              
                   .flatMapValues(x=>x.split(";"))                       //| .flatMap(x=>x._2.split(";").map((x._1,_,1)))
                   .map(x=>(x._1,x._2,1))  //output : (32615,insulin,1)  //| 
@@ -57,6 +59,11 @@ object DrugFrequencyByLocation {
 ////                                   .take(10)
 ////                                   .foreach(println)
 //                                   
+//                                   
+////(SEQN,DRUGNAME,1)
+////(73557,INSULIN,1)
+////(73558,GABAPENTIN,1)    
+//
 //       import spark.implicits._                        
 //       val dfMedications=rdd1.toDF("seqn","drugName","frequency")
 
